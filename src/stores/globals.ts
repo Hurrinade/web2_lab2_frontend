6f4db386e9b104f58e187fd023c28df041f075da
Hurrinade <marko.uremovic00@hotmail.com>
2022-11-06 09:51:02 +0100

    fixed url

--- src/stores/globals.ts
+++ src/stores/globals.ts
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 export const useGlobalsStore = defineStore('globals', {
     state: () => {
         return {
-            localUrl: "https://web2-labs.onrender.com",
+            // localUrl: "https://lab2-server.onrender.com",
             // localUrl: "https://localhost:4080",
             deployUrl: "https://localhost:4080",
             commentsData: [],
